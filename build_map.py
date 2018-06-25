b2f25797f084866f073becb25ae837b87898acfa
NanZhi Wang <wangnz1994@163.com>
2018-06-26 00:06:35 +0800

    for zuhui

--- build_map.py
+++ build_map.py
@@ -3,118 +3,132 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import math
 
-X=[]
-Y=[]
-for i in range(100000):
-    if i%5000==0:
-        print(i)
-    tmp=(np.random.random(3)-0.5)*6
-    x,y,z=tmp[0],tmp[1],tmp[2]
-    # if x>0.75 and y>0.75 and z>0.75:
-    #     X.append(tmp)
-    #     Y.append(0)
-    # if x<-0.75 and y>0.75 and z>0.75:
-    #     X.append(tmp)
-    #     Y.append(1)
-    # if x>0.75 and y<-0.75 and z>0.75:
-    #     X.append(tmp)
-    #     Y.append(2)
-    # if x<-0.75 and y<-0.75 and z>0.75:
-    #     X.append(tmp)
-    #     Y.append(3)
-    # if x>0.75 and y>0.75 and z<-0.75:
-    #     X.append(tmp)
-    #     Y.append(4)
-    # if x<-0.75 and y>0.75and z<-0.75:
-    #     X.append(tmp)
-    #     Y.append(5)
-    # if x>0.75 and y<-0.75 and z<-0.75:
-    #     X.append(tmp)
-    #     Y.append(6)
-    # if x<-0.75 and y<-0.75 and z<-0.75:
-    #     X.append(tmp)
-    #     Y.append(7)
-    if math.pow((x+2),2)+math.pow((y+2),2)+math.pow((z+2),2)<1:
-        X.append(tmp)
-        Y.append(0)
-    if math.pow((x+2),2)+math.pow((y-2),2)+math.pow((z+2),2)<1:
-        X.append(tmp)
-        Y.append(1)
-    if math.pow((x-2),2)+math.pow((y+2),2)+math.pow((z+2),2)<1:
-        X.append(tmp)
-        Y.append(2)
-    if math.pow((x-2),2)+math.pow((y-2),2)+math.pow((z+2),2)<1:
-        X.append(tmp)
-        Y.append(3)
-    if math.pow((x+2),2)+math.pow((y+2),2)+math.pow((z-2),2)<1:
-        X.append(tmp)
-        Y.append(4)
-    if math.pow((x+2),2)+math.pow((y-2),2)+math.pow((z-2),2)<1:
-        X.append(tmp)
-        Y.append(5)
-    if math.pow((x-2),2)+math.pow((y+2),2)+math.pow((z-2),2)<1:
-        X.append(tmp)
-        Y.append(6)
-    if math.pow((x-2),2)+math.pow((y-2),2)+math.pow((z-2),2)<1:
-        X.append(tmp)
-        Y.append(7)
+def genarete(style='ball'):
+    X=[]
+    Y=[]
+    if style=='ball':
+        for i in range(100000):
+            if i%5000==0:
+                print(i)
+            tmp=(np.random.random(3)-0.5)*6
+            x,y,z=tmp[0],tmp[1],tmp[2]
+            if math.pow((x+2),2)+math.pow((y+2),2)+math.pow((z+2),2)<1:
+                X.append(tmp)
+                Y.append(0)
+            if math.pow((x+2),2)+math.pow((y-2),2)+math.pow((z+2),2)<1:
+                X.append(tmp)
+                Y.append(1)
+            if math.pow((x-2),2)+math.pow((y+2),2)+math.pow((z+2),2)<1:
+                X.append(tmp)
+                Y.append(2)
+            if math.pow((x-2),2)+math.pow((y-2),2)+math.pow((z+2),2)<1:
+                X.append(tmp)
+                Y.append(3)
+            if math.pow((x+2),2)+math.pow((y+2),2)+math.pow((z-2),2)<1:
+                X.append(tmp)
+                Y.append(4)
+            if math.pow((x+2),2)+math.pow((y-2),2)+math.pow((z-2),2)<1:
+                X.append(tmp)
+                Y.append(5)
+            if math.pow((x-2),2)+math.pow((y+2),2)+math.pow((z-2),2)<1:
+                X.append(tmp)
+                Y.append(6)
+            if math.pow((x-2),2)+math.pow((y-2),2)+math.pow((z-2),2)<1:
+                X.append(tmp)
+                Y.append(7)
+        X=np.array(X)
+        Y=np.array(Y)
+        np.save('X_3' + '_' + style, X)
+        np.save('Y' + '_' + style, Y)
+    if style=='cubu':
+        X = []
+        Y = []
+        for i in range(100000):
+            if i % 5000 == 0:
+                print(i)
+            tmp = (np.random.random(3) - 0.5) * 6
+            x, y, z = tmp[0], tmp[1], tmp[2]
+            if x>0.75 and y>0.75 and z>0.75:
+                X.append(tmp)
+                Y.append(0)
+            if x<-0.75 and y>0.75 and z>0.75:
+                X.append(tmp)
+                Y.append(1)
+            if x>0.75 and y<-0.75 and z>0.75:
+                X.append(tmp)
+                Y.append(2)
+            if x<-0.75 and y<-0.75 and z>0.75:
+                X.append(tmp)
+                Y.append(3)
+            if x>0.75 and y>0.75 and z<-0.75:
+                X.append(tmp)
+                Y.append(4)
+            if x<-0.75 and y>0.75and z<-0.75:
+                X.append(tmp)
+                Y.append(5)
+            if x>0.75 and y<-0.75 and z<-0.75:
+                X.append(tmp)
+                Y.append(6)
+            if x<-0.75 and y<-0.75 and z<-0.75:
+                X.append(tmp)
+                Y.append(7)
+        X = np.array(X)
+        Y = np.array(Y)
+        np.save('X_3'+'_'+style, X)
+        np.save('Y'+'_'+style,Y)
+    if style=='huan':
+        X = []
+        Y = []
+        for i in range(1000000):
+            if i % 5000 == 0:
+                print(i)
+            tmp = (np.random.random(3) - 0.5) * 8
+            x, y, z = tmp[0], tmp[1], tmp[2]
+            if x*x+y*y<4 and x*x+y*y>2 and z>0.5 and z<2:
+                X.append(tmp)
+                Y.append(0)
+            if x*x+y*y+z*z<1:
+                X.append(tmp)
+                Y.append(1)
+        X = np.array(X)
+        Y = np.array(Y)
+        np.save('X_3'+'_'+style, X)
+        np.save('Y'+'_'+style,Y)
+
+def genareta_random_w():
+    w = np.random.random((3, 2))
+    np.save('w_random', w)
 
 
 
-
-
-
-def plot2():
-    w = np.random.random((3, 2)) * 2 - 1
-    np.save('w', w)
-    X = np.load('X_3.npy')
-    Y = np.load('Y.npy')
-    X=np.dot(X,w)
-    ax = plt.subplot(111)  # 创建一个三维的绘图工程
-    dic = {0: 'red', 1: 'green', 2: 'blue', 3: 'yellow', 4: 'black', 5: 'pink', 6: 'gray', 7: 'purple'}
-    for k in range(8):
-        X_ = []
-        for idx, i in enumerate(X):
-            if Y[idx] == k:
-                X_.append(i)
-        X_ = np.array(X_)
-        ax.scatter(X_[:, 0], X_[:, 1], s=10, c=dic[k])  # 绘制数据点
-
-    ax.set_ylabel('Y')
-    ax.set_xlabel('X')
-    #ax.view_init(elev=5, azim=10)
-    plt.show()
-
-
-def plot3(X,Y):
-    np.save('X_3',X)
-    X=np.array(X)
-    np.save('X_2',X)
-    np.save('Y',Y)
+def plot3(X_name,Y_name):
+    X = np.load(X_name + '.npy')
+    Y = np.load(Y_name + '.npy')
     ax = plt.subplot(111,projection='3d')  # 创建一个三维的绘图工程
     dic={0:'red',1:'green',2:'blue',3:'yellow',4:'black',5:'pink',6:'gray',7:'purple'}
-    for k in range(8):
-        X_=[]
+    for k in range(np.max(Y)+1):
+        X_tmp=[]
         for idx,i in enumerate(X):
             if Y[idx]==k:
-                X_.append(i)
-        X_=np.array(X_)
-        ax.scatter(X_[:,0], X_[:,1],X_[:,2] ,s=10,c=dic[k])  # 绘制数据点
-
+                X_tmp.append(i)
+        X_tmp=np.array(X_tmp)
+        ax.scatter(X_tmp[:,0], X_tmp[:,1],X_tmp[:,2] ,s=10,c=dic[k])  # 绘制数据点
     ax.set_zlabel('z')
     ax.set_ylabel('Y')
     ax.set_xlabel('X')
-    ax.view_init(elev=135, azim=30)
+    ax.view_init(elev=90, azim=90)
     plt.show()
 
-def plot2_best():
-    w = np.load('w_best.npy')
-    X = np.load('X_3.npy')
-    Y=np.load('Y.npy')
+
+def plot2(X_name,Y_name,w_name):
+    w = np.load(w_name+'.npy')
+    print(w)
+    X = np.load(X_name+'.npy')
+    Y=np.load(Y_name+'.npy')
     X = np.dot(X, w)
     ax = plt.subplot(111)  # 创建一个三维的绘图工程
     dic = {0: 'red', 1: 'green', 2: 'blue', 3: 'yellow', 4: 'black', 5: 'pink', 6: 'gray', 7: 'purple'}
-    for k in range(8):
+    for k in range(np.max(Y)+1):
         X_ = []
         for idx, i in enumerate(X):
             if Y[idx] == k:
@@ -125,6 +139,9 @@ def plot2_best():
     ax.set_xlabel('X')
     plt.show()
 
-#plot2()
-#plot3(X,Y)
-plot2_best()
+
+
+# genarete('huan')
+#genareta_random_w()
+# plot3('X_3_huan','Y_huan')
+plot2('X_3_huan','Y_huan','w_huan_nonlinear_best')
--- tensorflow_zuhui.py
+++ tensorflow_zuhui.py
@@ -2,44 +2,52 @@ import tensorflow as tf
 import random as rd
 import numpy as np
 
-X=np.load('X_3.npy')
-Y=np.load('Y.npy')
-X_test=X[:2000]
-
-X=X[2000:]
+X=np.load('X_3_huan.npy')
+print(X.shape)
+Y=np.load('Y_huan.npy')
+ss=int(len(X)*0.2)
+X_test=X[:ss]
+X=X[ss:]
 
 Y_tmp=[]
 for idx,i in enumerate(Y):
-    tmp=np.zeros(8)
+    tmp=np.zeros(2)
     tmp[i]=1
     Y_tmp.append(tmp)
 Y=np.array(Y_tmp)
-Y_test=Y[:2000]
-Y=Y[2000:]
-w1=tf.Variable(tf.random_uniform([3,2],minval=-0.5,maxval=0.5))
-w2=tf.Variable(tf.random_uniform([2,8],minval=-0.5,maxval=0.5))
+Y_test=Y[:ss]
+Y=Y[ss:]
+#w1=tf.Variable(tf.random_uniform([3,2],minval=-0.5,maxval=0.5))
+w1=tf.constant([[1,0],[0,1],[0,0]],dtype=tf.float32)
+w_y1=tf.Variable(tf.random_uniform([2,4],minval=-0.5,maxval=0.5))
+w_y2=tf.Variable(tf.random_uniform([4,2],minval=-0.5,maxval=0.5))
+w2=tf.Variable(tf.random_uniform([2,2],minval=-0.5,maxval=0.5))
 
 x=tf.placeholder(tf.float32,shape=(None,3))
-y_=tf.placeholder(tf.float32,shape=(None,8))
+y_=tf.placeholder(tf.float32,shape=(None,2))
 
 a=tf.matmul(x,w1)
-y=tf.matmul(a,w2)
 
-loss=tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_,logits=y)+tf.contrib.layers.l2_regularizer(0.001)(w1)
+b=tf.nn.relu(tf.matmul(a,w_y1))
+c=tf.nn.relu(tf.matmul(b,w_y2))
+y=tf.matmul(c,w2)
+
+loss=tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_,logits=y)#+tf.contrib.layers.l2_regularizer(0.001)(w1)
 
 train_step=tf.train.AdadeltaOptimizer(1).minimize(loss)
 
-batch_size=100
+batch_size=500
 with tf.Session() as sess:
     sess.run(tf.initialize_all_variables())
     STEP=100000
     for i in range(STEP):
         start=rd.randint(0,len(X)-1)
         end=min(len(X),start+batch_size)
+        # start = 0
+        # end = len(X)
         sess.run(train_step,feed_dict={x:X,y_:Y})
-        if STEP%10000==0:
+        if STEP%1000==0:
             y___=sess.run(y, feed_dict={x:X_test, y_:Y_test})
-
             y___=y___.tolist()
             r=0
             Y_test_=Y_test.tolist()
@@ -47,7 +55,7 @@ with tf.Session() as sess:
                if  Y_test_[idx].index(max(Y_test_[idx]))==y___[idx].index(max(y___[idx])):
                    r+=1
             print(np.mean(sess.run(loss, feed_dict={x:X_test, y_:Y_test})),r/len(Y_test))
-            np.save('w_best.npy',sess.run(w1))
+            np.save('w_huan_nonlinear_best.npy',sess.run(w1))
         #0.15456033 0.9425
 
 
